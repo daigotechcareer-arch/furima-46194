08f02edc28c03d52904a12f387dc6af59179ee36
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-10-18 21:43:40 +0900

    Userモデルからアソシエーション削除（ユーザー管理機能段階では不要）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # アソシエーション
-  has_many :items
-  has_many :orders
 
   # バリデーション
   validates :nickname, presence: true
