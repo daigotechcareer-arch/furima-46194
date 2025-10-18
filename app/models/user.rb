305881c3b37324277f993012b0e889b54676bb7e
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-10-18 16:52:11 +0900

    Rubocopによる自動整形完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,13 @@ class User < ApplicationRecord
 
   # バリデーション
   validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
-                                message: 'には英字と数字の両方を含めて設定してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   with_options presence: true do
-    validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :last_name_kana,  format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name_kana,  format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
     validates :birth_date
   end
 end
