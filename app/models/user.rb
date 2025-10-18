2b75f25564d3f19ed9c06407de5de2e9c6281c62
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-10-18 14:36:19 +0900

    ユーザー新規登録ページにエラーメッセージ表示機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
+# app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Deviseモジュール
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # アソシエーション
+  has_many :items
+  has_many :orders
+
+  # バリデーション
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                                message: 'には英字と数字の両方を含めて設定してください' }
+
+  with_options presence: true do
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :last_name_kana,  format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :birth_date
+  end
 end
