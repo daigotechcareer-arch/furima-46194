09ff1871f63434c272c561aa0ff8c77664e50cef
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-10-25 13:05:55 +0900

    Itemモデルにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,30 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :days_to_ship
+
+  # ===========================
+  # バリデーション設定
+  # ===========================
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
+
+  validates :price, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999,
+    message: "is out of setting range"
+  }
 end
