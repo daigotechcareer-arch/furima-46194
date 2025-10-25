3002c11a86c3e24ff92094be1e51e30de1a9b696
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-10-25 15:21:26 +0900

    販売手数料・販売利益の自動計算機能をJavaScriptで実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,24 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  if (!priceInput) return; // 商品出品ページ以外では動かないようにする
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    if (inputValue >= 300 && inputValue <= 9999999) {
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = Math.floor(inputValue - tax);
+      addTaxDom.innerHTML = tax;
+      profitDom.innerHTML = profit;
+    } else {
+      addTaxDom.innerHTML = '';
+      profitDom.innerHTML = '';
+    }
+  });
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
