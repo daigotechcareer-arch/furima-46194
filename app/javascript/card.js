869d3f9512b250ed22b8352522944713af76b5fe
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-11-08 14:46:45 +0900

    クレジットカード情報のトークン化処理を実装（PAY.JP連携）

---
+++ app/javascript/card.js
@@ -0,0 +1,37 @@
+// app/javascript/card.js
+
+const pay = () => {
+  const form = document.getElementById("charge-form");
+  if (!form) return;
+
+  // 公開鍵をセット
+  const payjp = Payjp(PAYJP_PUBLIC_KEY);
+  const elements = payjp.elements();
+
+  // 各入力フィールドを作成してHTMLの該当箇所にマウント
+  const numberElement = elements.create("cardNumber");
+  const expiryElement = elements.create("cardExpiry");
+  const cvcElement = elements.create("cardCvc");
+
+  numberElement.mount("#number-form");
+  expiryElement.mount("#expiry-form");
+  cvcElement.mount("#cvc-form");
+
+  // フォーム送信時の処理
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    payjp.createToken(numberElement).then((response) => {
+      if (response.error) {
+        alert("カード情報が正しくありません。");
+      } else {
+        const token = response.id;
+        const tokenInput = `<input value=${token} type="hidden" name="token">`;
+        form.insertAdjacentHTML("beforeend", tokenInput);
+        form.submit();
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
