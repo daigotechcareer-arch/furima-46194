159ea636014d1cf81419c828eab70ffc5d81b623
daigotechcareer-arch <daigo.tech.career@gmail.com>
2025-11-09 13:24:38 +0900

    売却済み商品の編集・削除ページへのアクセスを制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless current_user == @item.user
+    if current_user != @item.user || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def item_params
